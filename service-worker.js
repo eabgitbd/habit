1f4348461ccd949fa8c12b12536d6220962913e8
eabgitbd <emranahmed.matrix@gmail.com>
2026-07-14 02:44:45 +0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,10 @@
+const CACHE = 'habit-garden-shell-v2';
+const SHELL = ['./', './habit-garden.html', './manifest.json', './icon.svg'];
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL))));
+self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request).then(response => {
+    const copy = response.clone(); caches.open(CACHE).then(cache => cache.put(event.request, copy)); return response;
+  }).catch(() => caches.match('./habit-garden.html'))));
+});
